da8e96d09b39b67d4d3ed98a7798524a4f5c6f44
Mel-23200816 <l23200816@pachuca.tecnm.mx>
2026-02-22 00:22:12 -0600

    Adicion De Canva Curvas Cubicas De Bezier.

--- assests/js/main.js
+++ assests/js/main.js
@@ -141,7 +141,25 @@ function draw() {
   // Restauramos el eje a su posición original por si quieres dibujar más cosas después
   ctx.restore();
 
+  // =============================
+  // CURVAS CÚBICAS DE BÉZIER
+  // =============================
+  ctx.save();
+  ctx.translate(150, 200); // Fila inferior, al lado de la burbuja
+
+  ctx.beginPath();
+  ctx.moveTo(75, 40);
+  ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+  ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
+  ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
+  ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
+  ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
+  ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
   
+  ctx.fillStyle = "#8244C0"; // Morado intenso
+  ctx.fill();
+
+  ctx.restore();
 }
 
 function initUI() {
